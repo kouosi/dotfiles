a31920eadf87eb24c0a7c6c57cb8794c44aed311
kouosi <yamineko@tuta.io>
2023-12-31 14:41:17 +0545

    fix: Add simple script to link files like bashrc

---
+++ dontrun.sh
@@ -0,0 +1,30 @@
+set -xe
+# variables
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
+
+# make xdg dirs
+mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_CACHE_HOME
+mkdir -p $XDG_DATA_HOME
+mkdir -p $XDG_STATE_HOME
+
+# create files
+mkdir -p $XDG_STATE_HOME/bash
+touch $XDG_STATE_HOME/bash/history
+touch $XDG_DATA_HOME/gnupg
+touch $XDG_CONFIG_HOME/docker
+touch $XDG_DATA_HOME/wget-hsts
+mkdir -p $XDG_CONFIG_HOME/gdb
+touch $XDG_CONFIG_HOME/gdb/init
+mkdir ~/Pictures/Mpv/
+touch $XDG_CONFIG_HOME/ncmpcpp/error.log
+touch $XDG_CONFIG_HOME/mpd/playlists
+
+# link bash files
+ln ~/.config/bash/bashrc ~/.bashrc
+ln ~/.config/bash/bashrc ~/.bash_profile
+ln ~/.config/vim ~/.vim
+
