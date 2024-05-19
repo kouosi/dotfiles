1485ebb23aa52e90e33b6eda31b203c3e37bfeb4
kouosi <kouosi@tuta.io>
2024-05-19 23:32:37 +0545

    feat: Change theme to kanagawa

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,13 +1,24 @@
+-- local config = function()
+--     local theme = require("onedark")
+--     theme.setup{
+--         style = "darker"
+--     }
+--     theme.load()
+-- end
+
 local config = function()
-    local theme = require("onedark")
+    local theme = require("kanagawa")
     theme.setup{
-        style = "darker"
+        transparent = true,
+        dimInactive = true,
+        terminalColors = true,
     }
-    theme.load()
+    theme.load("wave")
 end
 
 local M = {
-    "navarasu/onedark.nvim",
+    -- "navarasu/onedark.nvim",
+    "rebelot/kanagawa.nvim",
     config = config,
     lazy   = false
 }
