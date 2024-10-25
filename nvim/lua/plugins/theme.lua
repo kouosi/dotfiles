2326efc1ab6302ff05bf676dd1ba9f468e083fb2
kouosi <kouosi@tuta.io>
2024-10-25 23:30:21 +0545

    feat: Start using sonokai colorscheme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,3 +1,4 @@
+-- Config for kanagawa
 -- local config = function()
 --     local theme = require("kanagawa")
 --     theme.setup{
@@ -8,22 +9,48 @@
 --     theme.load("wave")
 -- end
 
+-- Config for gruvbox
+-- local config = function()
+--     local theme = require("gruvbox")
+--     theme.setup {
+--         terminal_colors = true,
+--         contrast = "soft",
+--         dim_inactive = true,
+--         transparent_mode = true,
+--     }
+--     theme.load("gruvbox")
+--     -- vim.cmd("colorscheme gruvbox")
+-- end
+
+-- Config for everforest
+-- local config = function()
+--     local theme = require("everforest")
+--     theme.setup({
+--         background = "hard",
+--         transparent_background_level = 1,
+--         italics = true,
+--     })
+--     theme.load()
+-- end
+
+-- Config for shusia
 local config = function()
-    local theme = require("gruvbox")
-    theme.setup {
-        terminal_colors = true,
-        contrast = "soft",
-        dim_inactive = true,
-        transparent_mode = true,
-    }
-    vim.cmd("colorscheme gruvbox")
+  vim.cmd([[
+    let g:sonokai_style = 'shusia'
+    let g:sonokai_better_performance = 1
+    colorscheme sonokai
+    let g:lightline = {'colorscheme' : 'sonokai'}
+  ]])
 end
 
 local M = {
     -- "rebelot/kanagawa.nvim",
-    "ellisonleao/gruvbox.nvim",
+    -- "ellisonleao/gruvbox.nvim",
+    -- "neanias/everforest-nvim",
+    "sainnhe/sonokai",
     config = config,
-    lazy   = false
+    lazy   = false,
+    priority = 1000
 }
 
 return { M }
