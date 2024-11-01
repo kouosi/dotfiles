7f29503c5d1c9990e67058d2821fc9f406a36248
kouosi <kouosi@tuta.io>
2024-11-01 18:47:56 +0545

    feat: Add .local/bin

--- dontrun.sh
+++ dontrun.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
 set -xe
+
 # variables
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
-export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CONFIG_HOME=$HOME/.config/
+export XDG_CACHE_HOME=$HOME/.cache/
+export XDG_DATA_HOME=$HOME/.local/share/
+export XDG_STATE_HOME=$HOME/.local/state/
+export LOCAL_BIN=$HOME/.local/bin/
 
 # make xdg dirs
 mkdir -p $XDG_CONFIG_HOME
 mkdir -p $XDG_CACHE_HOME
 mkdir -p $XDG_DATA_HOME
 mkdir -p $XDG_STATE_HOME
+mkdir -p $LOCAL_BIN
 
 # create files
 mkdir -p $XDG_STATE_HOME/bash
@@ -23,6 +27,8 @@ mkdir -p ~/Pictures/Mpv/
 touch $XDG_CONFIG_HOME/ncmpcpp/error.log
 touch $XDG_CONFIG_HOME/mpd/playlists
 
+# install all
 stow config/ -t $XDG_CONFIG_HOME
 stow bash/ -t $HOME
-stow vim/ -t $HOME/.vim/
+# stow vim/ -t $HOME/.vim/
+stow lbin/ -t $LOCAL_BIN
