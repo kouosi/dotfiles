111d812348b23cc13c3d9ed126db8c42512b5069
kouosi <kouosi@tuta.io>
2025-04-10 15:02:38 +0545

    fix: Syntex for fold

--- config/nvim/lua/plugins/fold.lua
+++ config/nvim/lua/plugins/fold.lua
@@ -1,9 +1,9 @@
 local config = function()
     require('ufo').setup({
-    provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
-    end
-})
+        provider_selector = function(bufnr, filetype, buftype)
+            return {'treesitter', 'indent'}
+        end
+    })
 end
 
 local M = {
