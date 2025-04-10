8bde477e3e3c47794ecd3251cf1154059e64cf98
kouosi <kouosi@tuta.io>
2025-04-10 15:05:08 +0545

    fix: Remove all themes except that I use

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -19,36 +19,11 @@ local config = function()
         transparent_mode = true,
     }
     theme.load("gruvbox")
-    -- vim.cmd("colorscheme gruvbox")
 end
-
--- Config for everforest
--- local config = function()
---     local theme = require("everforest")
---     theme.setup({
---         background = "hard",
---         transparent_background_level = 1,
---         italics = true,
---     })
---     theme.load()
--- end
-
--- Config for shusia
--- local config = function()
---   vim.cmd([[
---     let g:sonokai_style = 'shusia'
---     let g:sonokai_better_performance = 1
---     let g:sonokai_transparent_background = 1
---     colorscheme sonokai
---     let g:lightline = {'colorscheme' : 'sonokai'}
---   ]])
--- end
 
 local M = {
     -- "rebelot/kanagawa.nvim",
     "ellisonleao/gruvbox.nvim",
-    -- "neanias/everforest-nvim",
-    -- "sainnhe/sonokai",
     config = config,
     lazy   = false,
     priority = 1000
