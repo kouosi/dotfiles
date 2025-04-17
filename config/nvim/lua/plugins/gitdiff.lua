15566310eb3208dfbaf116c029db302a046c9061
kouosi <kouosi@tuta.io>
2025-04-17 19:04:38 +0545

    feat(nvim): Add keymap to toggle and preview in gitdiff plugin

--- config/nvim/lua/plugins/gitdiff.lua
+++ config/nvim/lua/plugins/gitdiff.lua
@@ -9,6 +9,8 @@ local config = function()
             untracked    = { text = '?' },
         },
     })
+    vim.keymap.set('n', "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set('n', "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
 end
 
 local M = {
